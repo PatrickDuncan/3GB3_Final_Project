16497e5478160d3be96dea79720b004fbb0f4bb1
Patrick Diamond Duncan <patrick.duncan@outlook.com>
2017-01-26 21:02:38 -0500

    bullet progress

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -5,16 +5,13 @@ using UnityStandardAssets.CrossPlatformInput;
 public class Weapon : MonoBehaviour {
 
     public GameObject bullet;	// Assign the bullet prefab in the editor
-    public Camera camera;       // For the rotation
 
     void Update() {
-        Quaternion rot = GameObject.FindWithTag("MainCamera").transform.rotation;
-        Debug.Log(rot.x);
+        //Debug.Log(rot.x);
         // If the key is pressed create a game object (bullet) and then add a velocity
         if (Input.GetKeyDown(KeyCode.Mouse0)) {
-			GameObject gO = Instantiate(bullet, transform.position, Quaternion.identity) as GameObject;
-            gO.transform.rotation = Quaternion.Euler(rot.x, 0, 0);
-			gO.GetComponent<Rigidbody>().velocity = new Vector3(10, 0, 0);
+			GameObject gO = Instantiate(bullet, transform.position, transform.rotation) as GameObject;
+			gO.GetComponent<Rigidbody>().velocity = new Vector3(30, 0, 0);
 		}
     }
 }
