75c6c86a5a467ce287f7c67d7133a1f901e1abc7
Patrick Diamond Duncan <patrick.duncan@outlook.com>
2017-01-29 13:01:21 -0500

    timed shooting

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,11 +1,16 @@
 using System;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
+using System.Collections;
 
 public class Weapon : MonoBehaviour {
 
+    bool allowedToShoot = true;
+    float[] waitTimes = {1f, 0.2f, 1f, 1f};
+
     public GameObject bullet;	// Assign the bullet prefab in the editor
     public Camera cam;
+
     enum weapons {
         melee,
         pistol,
@@ -14,7 +19,9 @@ public class Weapon : MonoBehaviour {
     }
     weapons curWeapon = weapons.pistol;
 
-    void Update() {
+    private void Update() {
+        if (!allowedToShoot)
+            return;
         Vector3 wep;
         Vector3 position;
         switch(curWeapon) {
@@ -30,7 +37,15 @@ public class Weapon : MonoBehaviour {
         // If the key is pressed create a game object (bullet) and then add a velocity
         if (Input.GetKeyDown(KeyCode.Mouse0)) {
 			GameObject gO = Instantiate(bullet, position, Quaternion.Euler(90, 0, 0)) as GameObject;
-			gO.GetComponent<Rigidbody>().AddForce(transform.forward * 80);
+			gO.GetComponent<Rigidbody>().AddForce(transform.forward * 120);
+            StartCoroutine(Wait());
 		}
+    }
+
+    // You just used a weapon, wait to shoot again.
+	private IEnumerator Wait() {
+        allowedToShoot = false;
+        yield return new WaitForSeconds(waitTimes[(int) curWeapon]);
+     	allowedToShoot = true;
     }
 }
