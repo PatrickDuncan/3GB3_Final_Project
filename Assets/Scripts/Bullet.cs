fd08c6d3c16b95e6ddc49e03ac353b0e7d3ae877
Patrick Diamond Duncan <patrick.duncan@outlook.com>
2017-03-21 20:05:35 -0400

    Fixed issue where the bullet wouldn't reach an enemy if the player was in the air (was colliding with the player's body)

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -7,6 +7,7 @@ public class Bullet : MonoBehaviour {
 	}
 
 	void OnCollisionEnter(Collision col) {
-        Destroy(gameObject);    // auto destroy on impact
+		if (col.gameObject.tag != "Player")
+        	Destroy(gameObject);    // auto destroy on impact
 	}
 }
