3976e8e01ea4a624799af87e27cd5e3d7a4fcd62
Patrick Diamond Duncan <patrick.duncan@outlook.com>
2017-03-23 14:51:28 -0400

    can't toggle flashlight if dead

--- Assets/Scripts/Flashlight.cs
+++ Assets/Scripts/Flashlight.cs
@@ -5,11 +5,16 @@ public class Flashlight : MonoBehaviour {
 
     bool off;
 
+    PlayerHealth player;
+
+    void Start() {
+        player = GameObject.FindWithTag("Player").GetComponent<PlayerHealth>();
+    }
+
     void Update() {
-        if (Input.GetKeyDown(KeyCode.F)) {  // on and off
+        if (Input.GetKeyDown(KeyCode.F) && player.GetHealth() > 0) {
             GetComponent<Light>().intensity = (off ? 8 : 0);
             off = !off;
         }
     }
-
 }
