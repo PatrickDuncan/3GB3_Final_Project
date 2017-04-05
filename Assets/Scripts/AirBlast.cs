96e7ff4bb5badbae7792f7e06fbba7f2524413b1
Patrick Diamond Duncan <patrick.duncan@outlook.com>
2017-04-05 06:35:09 -0400

    fixed bug where the player could look up and air jump

--- Assets/Scripts/AirBlast.cs
+++ Assets/Scripts/AirBlast.cs
@@ -28,25 +28,28 @@ public class AirBlast : MonoBehaviour {
         // If the key is pressed create a game object (wall) and then add a velocity
         if (Input.GetKeyDown(KeyCode.Mouse1)) {
             anim.SetTrigger(blastAnim);
-            // Air blast in front of you
-            if (myTransform.rotation.eulerAngles.x < 60) {
-                Vector3 position = GameObject.FindWithTag("AirBlastOrigin").transform.position;
-                Quaternion rotation = GameObject.FindWithTag("MainCamera").transform.rotation;
-                GameObject gO = Instantiate(airWall, position, rotation) as GameObject;
-    			gO.GetComponent<Rigidbody>().AddForce(myTransform.forward * 500000);
-                Destroy(gO, DISPPEAR_TIME);
-            }
-            // air blast jump
-            else {
-                Rigidbody rb = GameObject.FindWithTag("Player").GetComponent<Rigidbody>();
-                rb.drag = 0f;
-                // force doesnt register when grounded
-                rb.velocity = new Vector3(rb.velocity.x, 1f, rb.velocity.z);
-                rb.AddForce(new Vector3(0f, 400, 0f), ForceMode.Impulse);
-            }
+            float x = myTransform.rotation.eulerAngles.x;
+            if (x < 90 && x > 60) AirJump();
+            else                  AirPush();
             shootSound.Play();
             StartCoroutine(WaitToShoot());
 		}
+    }
+
+    void AirPush() {
+        Vector3 position = GameObject.FindWithTag("AirBlastOrigin").transform.position;
+        Quaternion rotation = GameObject.FindWithTag("MainCamera").transform.rotation;
+        GameObject gO = Instantiate(airWall, position, rotation) as GameObject;
+        gO.GetComponent<Rigidbody>().AddForce(myTransform.forward * 500000);
+        Destroy(gO, DISPPEAR_TIME);
+    }
+
+    void AirJump() {
+        Rigidbody rb = GameObject.FindWithTag("Player").GetComponent<Rigidbody>();
+        rb.drag = 0f;
+        // force doesnt register when grounded
+        rb.velocity = new Vector3(rb.velocity.x, 1f, rb.velocity.z);
+        rb.AddForce(new Vector3(0f, 400, 0f), ForceMode.Impulse);
     }
 
     // You just used a weapon, wait to shoot again.
